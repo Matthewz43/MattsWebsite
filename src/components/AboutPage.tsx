779cd62a0fa6f87665a4c7b36b33dd681a06ba3d
zycata <193771833+Matthewz43@users.noreply.github.com>
2025-08-25 23:06:38 -0700

    Tweaked css properties

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -25,7 +25,7 @@ const Section: React.FC<SectionProps> = ({ color, children }) => {
         use this to center it and then add container fluid to "increase size of class"
         OKAY CHANGE text-center TO SOMEWHERE ELSE IT KINDA RUINS SHIT
       */}
-      <div className="bg-warning container text-white text-center">{children}</div>
+      <div className="bg-warning container text-white ">{children}</div>
     </div>
   );
 };
