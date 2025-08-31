95f3630279cb0037f7793de0b156da15660a3c86
matthewz43 <193771833+Matthewz43@users.noreply.github.com>
2025-08-30 23:42:18 -0700

    Added anims

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+
 // import 'bootstrap/dist/css/bootstrap.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css'
+import './styles/book.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
